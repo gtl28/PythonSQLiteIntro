7cd82a5c8ac82e39d7c7cd7a915b102673e18d14
George Luckett <gtl28@bath.ac.uk>
2025-04-17 21:16:25 +0000

    end of day 3

--- main.py
+++ main.py
@@ -34,40 +34,40 @@ for tableName in tableNames:
 # Create the Airport table
 conn.execute("""
 CREATE TABLE Airport (
-    'Airport ID' INTEGER PRIMARY KEY AUTOINCREMENT,
-    'Airport Name' TEXT,
+    Airport_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    Airport_Name TEXT,
     Latitude FLOAT,
     Longitude FLOAT,
-    'Time Difference' INTEGER
+    Time_Difference INTEGER
 );
 """)
 
 # Create the Pilot table
 conn.execute("""
 CREATE TABLE Pilot (
-    'Pilot ID' INTEGER PRIMARY KEY AUTOINCREMENT,
-    'Pilot Name' TEXT,
-    'Pilot Age' INTEGER,
-    'Flight Experience' INTEGER
+    Pilot_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    Pilot_Name TEXT,
+    Pilot_Age INTEGER,
+    Flight_Experience INTEGER
 );
 """)
 
 # Create the Plane table
 conn.execute("""
 CREATE TABLE Plane (
-    'Plane ID' INTEGER PRIMARY KEY AUTOINCREMENT,
-    'Plane Name' TEXT,
-    'Cargo Capacity' INTEGER,
-    'People Capacity' INTEGER
+    Plane_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    Plane_Name TEXT,
+    Cargo_Capacity INTEGER,
+    People_Capacity INTEGER
 );
 """)
 
 # Create the Schedule table
 conn.execute("""
 CREATE TABLE Schedule (
-    'Schedule ID' INTEGER PRIMARY KEY AUTOINCREMENT,
-    'Planned Datetime' TEXT,
-    'Actual Datetime' TEXT,
+    Schedule_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    Planned_Datetime TEXT,
+    Actual_Datetime TEXT,
     Status TEXT
 );
 """)
@@ -75,25 +75,25 @@ CREATE TABLE Schedule (
 # Create the Flight table
 conn.execute("""
 CREATE TABLE Flight (
-    'Flight ID' INTEGER PRIMARY KEY AUTOINCREMENT,
-    'Origin Airport ID' INTEGER,
-    'Destination Airport ID' INTEGER,
-    'Departure Schedule ID' INTEGER,
-    'Arrival Schedule ID' INTEGER,
-    'Pilot ID' INTEGER,
-    'Plane ID' INTEGER,
-    FOREIGN KEY ('Origin Airport ID') REFERENCES Airport('Airport ID'),
-    FOREIGN KEY ('Destination Airport ID') REFERENCES Airport('Airport ID'),
-    FOREIGN KEY ('Departure Schedule ID') REFERENCES Schedule('Schedule ID'),
-    FOREIGN KEY ('Arrival Schedule ID') REFERENCES Schedule('Schedule ID'),
-    FOREIGN KEY ('Pilot ID') REFERENCES Pilot('Pilot ID'),
-    FOREIGN KEY ('Plane ID') REFERENCES Plane('Plane ID')
+    Flight_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    Origin_Airport_ID INTEGER,
+    Destination_Airport_ID INTEGER,
+    Departure_Schedule_ID INTEGER,
+    Arrival_Schedule_ID INTEGER,
+    Pilot_ID INTEGER,
+    Plane_ID INTEGER,
+    FOREIGN KEY (Origin_Airport_ID) REFERENCES Airport(Airport_ID),
+    FOREIGN KEY (Destination_Airport_ID) REFERENCES Airport(Airport_ID),
+    FOREIGN KEY (Departure_Schedule_ID) REFERENCES Schedule(Schedule_ID),
+    FOREIGN KEY (Arrival_Schedule_ID) REFERENCES Schedule(Schedule_ID),
+    FOREIGN KEY (Pilot_ID) REFERENCES Pilot(Pilot_ID),
+    FOREIGN KEY (Plane_ID) REFERENCES Plane(Plane_ID)
 );
 """)
 
 # Populate the Airport table
 conn.execute("""
-INSERT INTO Airport ('Airport Name', Latitude, Longitude, 'Time Difference') VALUES
+INSERT INTO Airport (Airport_Name, Latitude, Longitude, Time_Difference) VALUES
 ('Los Angeles International', 33.9416, -118.4085, -8),
 ('John F. Kennedy International', 40.6413, -73.7781, -5),
 ('Heathrow', 51.4700, -0.4543, 0),
@@ -108,7 +108,7 @@ INSERT INTO Airport ('Airport Name', Latitude, Longitude, 'Time Difference') VAL
 
 # Populate the Pilot table
 conn.execute("""
-INSERT INTO Pilot ('Pilot Name', 'Pilot Age', 'Flight Experience')
+INSERT INTO Pilot (Pilot_Name, Pilot_Age, Flight_Experience)
 VALUES 
     ('John Smith', 45, 2000),
     ('Alice Johnson', 38, 1500),
@@ -124,7 +124,7 @@ VALUES
 
 # Populate the Schedule table
 conn.execute("""
-INSERT INTO Schedule ('Planned Datetime', 'Actual Datetime', 'Status') VALUES 
+INSERT INTO Schedule (Planned_Datetime, Actual_Datetime, Status) VALUES 
 ('2025-04-16 08:00:00', '2025-04-16 08:00:00', 'On Time'),
 ('2025-04-16 15:00:00', '2025-04-16 23:00:00', 'Cancelled'),
 ('2025-04-17 10:00:00', '2025-04-17 11:00:00', 'Delayed'),
@@ -139,7 +139,7 @@ INSERT INTO Schedule ('Planned Datetime', 'Actual Datetime', 'Status') VALUES
 
 # Populate the Plane table
 conn.execute("""
-INSERT INTO Plane ('Plane Name', 'Cargo Capacity', 'People Capacity')
+INSERT INTO Plane (Plane_Name, Cargo_Capacity, People_Capacity)
 VALUES 
     ('Boeing 747', 15000, 416),
     ('Airbus A380', 13000, 555),
@@ -155,7 +155,7 @@ VALUES
 
 # Populate the Flight table
 conn.execute("""
-INSERT INTO Flight ('Origin Airport ID', 'Destination Airport ID', 'Departure Schedule ID', 'Arrival Schedule ID', 'Pilot ID', 'Plane ID')
+INSERT INTO Flight (Origin_Airport_ID, Destination_Airport_ID, Departure_Schedule_ID, Arrival_Schedule_ID, Pilot_ID, Plane_ID)
 VALUES 
     (1, 2, 1, 2, 1, 3),
     (3, 4, 3, 4, 8, 3),
@@ -184,13 +184,14 @@ def printTable(name):
 runApplication = True
 
 def select_option(option_type, options, return_type):
-    print("Please select a " + option_type)
+    print("Please select a " + option_type + " Option Number")
     print("Your options are: ")
+    options = [str(option) for option in options]
     for i in range(1, len(options)+1):
         print("Option " + str(i) + " : " + options[i-1])
     option_selected = False
     while not option_selected:
-        option_index = input("\nSelect " + option_type + " : ")
+        option_index = input("\nSelect " + option_type + " Option Number : ")
         if option_index in [str(x) for x in range(1, len(options)+1)]:
             print("Option " + option_index + " : " + options[int(option_index) - 1] + " Selected\n")
             if return_type == "index":
@@ -202,7 +203,7 @@ def select_option(option_type, options, return_type):
 
 
 # Creation
-def add_item(table_name):
+def get_fields(table_name):
     fields = []
     query = "PRAGMA table_info(" + tableName + ")"
     cursor.execute(query)
@@ -211,15 +212,72 @@ def add_item(table_name):
     for field in fields:
         print(field)
     print("\n")
-
-    select_option()
+    return fields
     #keep it simple as and just select row from each fields
+
+# Creation
+def add_item_to_table(tableName):
+    fields = get_fields(tableName)[1:]
+    item = []
+
+    for field in fields:
+        query = "SELECT DISTINCT " + field + " FROM " + tableName
+        cursor.execute(query)
+        options = [row[0] for row in cursor.fetchall()]
+        choice = select_option(field, options, "element")
+        item.append(choice)
+
+    print("Item to Add: " + str(item))
+    fields_string = ", ".join(fields)
+    item_values = ", ".join(item)
+    query = "INSERT INTO " + tableName + " (" + fields_string + ") VALUES (" + item_values + ")"
+    cursor.execute(query)
+    print("Item added to " + tableName)
+
+# Creation
+def add_flight():
+    tableName = "Flight"
+
+    fields = get_fields(tableName)[1:]
+    item = []
+
+    for field in fields:
+        print(field)
+        query = "SELECT DISTINCT " + field + " FROM " + tableName
+        cursor.execute(query)
+        options = [row[0] for row in cursor.fetchall()]
+        match field:
+            case "Origin_Airport_ID":
+                printTable("Airport")
+            case "Destination_Airport_ID":
+                printTable("Airport")
+            case "Departure_Schedule_ID":
+                printTable("Schedule") 
+            case "Arrival_Schedule_ID":
+                printTable("Schedule")
+            case "Pilot_ID": 
+                printTable("Pilot")
+            case "Plane_ID":
+                printTable("Plane")
+        choice = select_option(field, options, "element")
+        item.append(choice)
+
+    print("Item to Add: " + str(item))
+    fields_string = ", ".join(fields)
+    item_values = ", ".join(item)
+    query = "INSERT INTO " + tableName + " (" + fields_string + ") VALUES (" + item_values + ")"
+    cursor.execute(query)
+    print("New item added to " + tableName + " table.")
+    print("The updated Flight table is below.")
+    printTable("Flight")
+
 # Creation
 def add_pilot():
-    print("Pilot Added")
+    print("Pilot added")
 
 # Read
 def view_flight():
+
     print("Flight Viewed")
 
 # Read
@@ -241,17 +299,49 @@ def update_destination():
 
 # Deletion
 def remove_pilot():
-    print("Pilot Removed")
+    print("This is the current Pilot table")
+    printTable("Pilot")
+    print("Please select the Pilot ID of the Pilot you wish to remove")
+    query = "SELECT DISTINCT Pilot_ID FROM Pilot"
+    cursor.execute(query)
+    options = [row[0] for row in cursor.fetchall()]
+    id_to_delete = select_option("Pilot ID", options, "element")
+    query = "DELETE FROM Pilot WHERE Pilot_ID = " + id_to_delete
+    cursor.execute(query)
+    query = "DELETE FROM Flight WHERE Pilot_ID = " + id_to_delete
+    cursor.execute(query)
+    print("This is the updated Pilot table after a row was deleted")
+    printTable("Pilot")
 
 # Deletion
 def remove_flight():
-    print("Flight")
+    print("This is the current Flight table")
+    printTable("Flight")
+    print("Please select the Flight ID of the Flight you wish to remove")
+    query = "SELECT DISTINCT Flight_ID FROM Flight"
+    cursor.execute(query)
+    options = [row[0] for row in cursor.fetchall()]
+    id_to_delete = select_option("Flight ID", options, "element")
+    query = "DELETE FROM Flight WHERE Flight_ID = " + id_to_delete
+    cursor.execute(query)
+    print("This is the updated Flight table after a row was deleted")
+    printTable("Flight")
+    
+def view_all_tables():
+    for table in tableNames:
+        printTable(table)
 
 print("Welcome to the Flight Management Database Application.")
 print("The database has been reset and filled with example data.")
+
+print("All the tables in this database are shown below.")
+view_all_tables()
+
 print("You many perform various CRUD operation on this data using the actions available in the Actions Menu")
 
-actions = ["(Create) Add a new flight", "(Create) Add a new pilot", "(Read) View flight information", "(Read) View pilot schedule", "(Read) View table", "(Update) Update flight information", "(Update) Update destination information", "(Delete) Remove pilot", "(Delete) Remove flight"]
+
+
+actions = ["(Create) Add a new flight", "(Create) Add a new pilot", "(Read) View flight information", "(Read) View pilot schedule", "(Read) View table", "(Update) Update flight information", "(Update) Update destination information", "(Delete) Remove pilot", "(Delete) Remove flight", "(View) View all tables"]
 tables = []
 
 while runApplication:
@@ -260,7 +350,7 @@ while runApplication:
     
     match menuChoice:
         case 1:
-            add_item("Flight")
+            add_flight()
         case 2:
             add_pilot()
         case 3:
@@ -277,6 +367,8 @@ while runApplication:
             remove_pilot()
         case 9:
             remove_flight()
+        case 10:
+            view_all_tables()
 
 cursor.close()
 conn.close()
